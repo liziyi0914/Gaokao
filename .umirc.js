7292c3fdb61d535515f1cac7b838a584d80ea037
liziyi0914 <liziyi0914@gmail.com>
2020-03-31 12:30:50 +0800

    support gh-page

--- .umirc.js
+++ .umirc.js
@@ -2,6 +2,7 @@
 // ref: https://umijs.org/config/
 export default {
 	history: 'hash',
+	publicPath: '/gaokao/',
 	treeShaking: true,
 	plugins: [
 		// ref: https://umijs.org/plugin/umi-plugin-react.html
