55e85439acdc0bf56cd801cfbe4c7789c70ea030
liziyi0914 <liziyi0914@gmail.com>
2020-04-29 02:31:40 +0800

    fix path

--- .umirc.js
+++ .umirc.js
@@ -2,7 +2,7 @@
 // ref: https://umijs.org/config/
 export default {
 	history: 'hash',
-	publicPath: '/Gaokao/',
+	publicPath: '/',
 	treeShaking: true,
 	plugins: [
 		// ref: https://umijs.org/plugin/umi-plugin-react.html
